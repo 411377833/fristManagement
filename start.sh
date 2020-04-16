3c8678a7339a325d9a89882f9fabd32322310b0a
jinxin <411377833@qq.com>
2020-04-16 09:55:07 +0800

    发版

--- start.sh
+++ start.sh
@@ -2,10 +2,12 @@
 git pull
 
 # 安装依赖，打包
-yarn --registry=https://registry.npm.taobao.org/ && yarn build
+# yarn --registry=https://registry.npm.taobao.org/ && yarn build
+# 打包
+yarn build
 
 #删除容器
-# docker rm -f demo1 &> /dev/null
+docker rm -f fristManagement &> /dev/null
 
 #启动容器
 docker run -d --restart=on-failure:5\
